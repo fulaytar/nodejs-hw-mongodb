ed0b1edc6bfcfebdd83eff7aa955c29784686458
fulaytar <fulaytar2405@gmail.com>
2024-06-24 00:17:01 +0300

    add next()

--- src/middlewares/NotFoundHandler.js
+++ src/middlewares/NotFoundHandler.js
@@ -1,7 +1,8 @@
-const notFoundHandler = (req, res) => {
+const notFoundHandler = (req, res, next) => {
   res.status(404).json({
     message: 'Route not found',
   });
+  next();
 };
 
 export default notFoundHandler;
