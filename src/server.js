6e12cf9dbf5e070bed5a9082bb19cae544c92246
fulaytar <fulaytar2405@gmail.com>
2024-07-05 01:07:21 +0300

    fix logger

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const port = env('PORT', '3000');
 const setupServer = () => {
   const app = express();
 
-  //app.use(logger);
+  app.use(logger);
   app.use(express.json()); // don`t forget
   app.use(cors());
 
